a77d2f72d4e3bdc08b5702d0d60da5b7d340b6b5
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2023-05-23 11:09:52 +0530

    basic code

---
+++ Week 12/Q1.c
@@ -0,0 +1,82 @@
+/*
+1)Write a C program to merge contents of two files into a third file.(3Marks)
+
+Input:
+gedit file1.txt 
+Hi,Good morning!
+Have a nice day 
+
+gedit file2.txt
+Welcome to C programming- file handling concepts
+
+gedit file3.txt
+//empty file
+
+Enter the 1st file name : file1.txt Enter the 2nd file name : file2.txt
+Enter the new file name to merge the two files:file3.txt
+
+Output:
+The two files merged into file3.txt file successfully..!!
+//Third file-Merged contents of two files(file1.txt and file2.txt)
+
+gedit file3.txt
+Hi,Good morning!
+Have a nice day
+Welcome to C programming- file handling concepts
+*/
+
+//Solution:
+#include <stdio.h>
+
+int main() {
+    FILE *file1, *file2, *file3;
+    char file1_name[100], file2_name[100], file3_name[100];
+    char ch;
+
+    printf("Enter the 1st file name: ");
+    scanf("%s", file1_name);
+
+    printf("Enter the 2nd file name: ");
+    scanf("%s", file2_name);
+
+    printf("Enter the new file name to merge the two files: ");
+    scanf("%s", file3_name);
+
+    // Open the first file in read mode
+    file1 = fopen(file1_name, "r");
+    if (file1 == NULL) {
+        printf("Unable to open %s\n", file1_name);
+        return 1;
+    }
+
+    // Open the second file in read mode
+    file2 = fopen(file2_name, "r");
+    if (file2 == NULL) {
+        printf("Unable to open %s\n", file2_name);
+        return 1;
+    }
+
+    // Open the third file in write mode
+    file3 = fopen(file3_name, "w");
+    if (file3 == NULL) {
+        printf("Unable to create %s\n", file3_name);
+        return 1;
+    }
+
+    // Merge the contents of the first and second files into the third file
+    while ((ch = fgetc(file1)) != EOF) {
+        fputc(ch, file3);
+    }
+    while ((ch = fgetc(file2)) != EOF) {
+        fputc(ch, file3);
+    }
+
+    printf("The two files merged into %s successfully..!!\n", file3_name);
+
+    // Close all the files
+    fclose(file1);
+    fclose(file2);
+    fclose(file3);
+
+    return 0;
+}
